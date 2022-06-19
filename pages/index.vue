afaf0aa5edf162f700e35db1bd897bee54c212d5
Juan Alba Harrington <juan.harrington@zeneducate.com>
2022-06-19 00:56:17 -0300

    4 - moving to nuxt and fixing tests

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,11 @@
     <div v-else>
       <h1>Nuxt Genres</h1>
       <ul>
-        <li v-for="(genre,index) in genresData.genres" :key="index">{{ genre.name }}</li>  
+        <li v-for="(genre, index) in genresData.genres" :key="index">
+          <nuxt-link :to="`/category?id=${genre.id}`">
+            {{ genre.name }}
+          </nuxt-link>
+        </li>
       </ul>
       <button @click="$fetch">Refresh</button>
     </div>
@@ -14,21 +18,23 @@
 
 
 <script>
+
 export default {
   name: 'IndexPage',
   data() {
-      return {
-        genresData: {}
-      }
-    },
+    return {
+      genresData: {},
+    }
+  },
   async fetch() {
     this.genresData = await fetch(
       `${this.$config.baseUrl}/genre/movie/list?api_key=${this.$config.apiSecret}&language=en-US`
-    )
-    .then(res => {
+    ).then((res) => {
       return res.json()
     })
   },
-
 }
+
+
+
 </script>
--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -2,10 +2,19 @@ import { mount } from '@vue/test-utils'
 import IndexPage from '@/pages/index.vue'
 
 describe('IndexPage', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(IndexPage)
+  test('is a Vue instance', async () => {
+    const wrapper = mount(IndexPage, {
+      mocks: {
+        $fetchState: { pending: false, error: false },
+        genresData: [{ id: 1, name: 'cat1' }],
+      },
+    })
+
+    IndexPage.data = () => [{ id: 1, name: 'cat1' }]
+    IndexPage.fetch = () => [{ id: 1, name: 'cat1' }]
+
     const div = wrapper.find('div')
     expect(wrapper.vm).toBeTruthy()
-    expect(div.text()).toContain('Home View')
+    expect(div.text()).toContain('Nuxt Genres')
   })
 })
