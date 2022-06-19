4a35d2aba70678b13124bba28de7a4d5479ddf13
Juan Alba Harrington <juan.harrington@zeneducate.com>
2022-06-19 01:27:05 -0300

    5 - Getting info from api

--- test/pages/item.spec.js
+++ test/pages/item.spec.js
@@ -4,7 +4,7 @@ import ItemPage from '@/pages/item.vue'
 describe('ItemPage', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(ItemPage)
-    const div = wrapper.find('div')
+    const div = wrapper.find('h1')
     expect(wrapper.vm).toBeTruthy()
     expect(div.text()).toContain('Item View')
   })
